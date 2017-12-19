e798231ec2d46ca8d1c1d4eafb686a36c8a2ed4b
Dmitry Deshevoy <mityada@gmail.com>
2017-12-19 16:18:00 +0300

    Update XMonad configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,11 +6,14 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Prompt
 import XMonad.Prompt.Shell
+import XMonad.Actions.SpawnOn
 
 import Data.Ratio ((%))
 import qualified Data.Map as M
@@ -18,7 +21,7 @@ import qualified Data.Map as M
 import IM
 
 main = do
-    xmonad =<< statusBar "xmobar" myPP toggleStrutsKey desktopConfig
+    xmonad =<< statusBar "xmobar" myPP toggleStrutsKey (withUrgencyHook NoUrgencyHook desktopConfig
         { borderWidth        = 1
         , normalBorderColor  = "#113354"
         , focusedBorderColor = "#6587a8"
@@ -27,18 +30,23 @@ main = do
         , manageHook         = myManageHook
         , layoutHook         = smartBorders $ avoidStruts $ myLayoutHook
         , startupHook        = myStartupHook
-        , logHook            = takeTopFocus } 
+        , logHook            = takeTopFocus
+        , handleEventHook    = docksEventHook <+> fullscreenEventHook } )
 
 myKeys (XConfig {modMask = modm}) = M.fromList $
-    [ ((modm, xK_p), shellPrompt myXPConfig)
+    [ ((modm, xK_p), shellPromptHere myXPConfig)
+    , ((modm, xK_x), spawn "light-locker-command -l")
+    , ((0, xK_Print), spawn "import -window root ~/Pictures/screenshots/$(date '+%Y%m%d-%H%M%S').png")
     -- XF86AudioLowerVolume
-    , ((0, 0x1008ff11), spawn "amixer -q set Master 1-")
+    , ((0, 0x1008ff11), spawn "amixer -q set Master 1%-")
     -- XF86AudioRaiseVolume
-    , ((0, 0x1008ff13), spawn "amixer -q set Master 1+ unmute")
+    , ((0, 0x1008ff13), spawn "amixer -q set Master 1%+ unmute")
+    -- XF86AudioMute
+    , ((0, 0x1008ff12), spawn "amixer -q set Master toggle")
     -- XF86AudioPlay
-    , ((0, 0x1008ff14), spawn "mocp -U")
+    , ((0, 0x1008ff14), spawn "mocp-control play")
     -- XF86AudioStop
-    , ((0, 0x1008ff15), spawn "mocp -P")
+    , ((0, 0x1008ff15), spawn "mocp-control stop")
     -- XF86AudioPrev
     , ((0, 0x1008ff16), spawn "mocp -r")
     -- XF86AudioNext
@@ -46,23 +54,24 @@ myKeys (XConfig {modMask = modm}) = M.fromList $
 
 myManageHook = composeAll
     [ isFullscreen                  --> doFullFloat
-    , className =? "Konsole"        --> doShift "1"
+    , className =? "xterm"          --> doShift "1"
     , className =? "Firefox"        --> doShift "2"
     , className =? "Skype"          --> doShift "3"
-    , className =? "psi"            --> doShift "3"
-    , className =? "Plasma-desktop" --> doFloat
-    , className =? "Plasma"         --> doIgnore
-    , className =? "Klipper"        --> doFloat
-    , className =? "stalonetray"    --> doIgnore
-    , className =? "Wicd-kde" --> doFloat ]
+    , className =? "Psi-plus"       --> doShift "3"
+    , className =? "qTox"           --> doShift "3"
+    , className =? "ViberPC"        --> doShift "3"
+    , className =? "Logic"          --> doIgnore ]
+    -- , className =? "Gimp"           --> doFloat ]
 
-myLayoutHook = onWorkspace "3" (gridIM (1%6) rosterProperty) $ layoutHook desktopConfig
+myLayoutHook = onWorkspace "3" (reflectHoriz $ gridIM (1%6) rosterProperty) $ layoutHook desktopConfig
 
-rosterProperty = [psiRoster, skypeRoster]
+rosterProperty = [psiRoster, skypeRoster, toxRoster]
 
-psiRoster = (And (ClassName "psi") (Resource "main"))
+psiRoster = (And (ClassName "Psi-plus") (Resource "main"))
 
 skypeRoster = (And (ClassName "Skype") (Title "mityada - Skype™"))
+
+toxRoster = (And (ClassName "qTox") (Title "qTox"))
 
 myStartupHook = do
     setWMName "LG3D"
